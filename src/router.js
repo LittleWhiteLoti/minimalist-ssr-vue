a06816dc1825b2c9fc0c4f91b5d7f2e749257e2c
Nathan Thomas <thomanat@iu.edu>
2022-04-11 18:05:37 -0400

    Switching tabs to spaces in router.js

--- src/router.js
+++ src/router.js
@@ -21,11 +21,11 @@ const routes = Object
 
 export function createRouter() 
 {
-  	return _createRouter(
-	{
-    	// use appropriate history implementation for server/client
-    	// import.meta.env.SSR is injected by Vite.
-    	history: import.meta.env.SSR ? createMemoryHistory() : createWebHistory(),
-    	routes
-  	})
+    return _createRouter(
+    {
+        // use appropriate history implementation for server/client
+        // import.meta.env.SSR is injected by Vite.
+        history: import.meta.env.SSR ? createMemoryHistory() : createWebHistory(),
+        routes
+    })
 }
